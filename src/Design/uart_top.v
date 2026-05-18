5d771c8306e2126c8834f965c464dc597d994c48
VigneshCShet <vigneshshet1@gmail.com>
2026-05-18 16:06:01 +0530

    initial Commit

---
+++ src/Design/uart_top.v
@@ -0,0 +1,15 @@
+`default_nettype none
+module top_uart #(parameter data_width = 8)(sys_clk, sys_rst_l, xmitH, xmit_dataH, uart_REC_dataH, uart_XMIT_dataH, xmit_doneH, rec_readyH, rec_dataH, rec_busy, xmit_active);
+  input wire sys_clk, sys_rst_l, xmitH, uart_REC_dataH;
+  input wire [data_width - 1 : 0] xmit_dataH;
+  output wire uart_XMIT_dataH, xmit_doneH, rec_readyH, rec_busy, xmit_active;
+  output wire [data_width - 1 : 0] rec_dataH;
+  
+  wire synch_in;
+  wire baud_clk;
+  synchronizer u4 (.clk(sys_clk), .rst(sys_rst_l), .sin(uart_REC_dataH), .sout(synch_in));
+  baud_clk u3 (.clk(sys_clk), .rst(sys_rst_l), .clk_out(baud_clk));
+  xmit u1(.baud_clk(baud_clk), .sys_rst_l(sys_rst_l), .xmitH(xmitH), .xmit_dataH(xmit_dataH), .uart_XMIT_dataH(uart_XMIT_dataH), .xmit_doneH(xmit_doneH), .xmit_active(xmit_active));
+  rec u2 (.clk(baud_clk), .rst(sys_rst_l), .data_in(synch_in), .rec_readyH(rec_readyH), .rec_dataH(rec_dataH), .rec_busyH(rec_busy));
+
+endmodule
