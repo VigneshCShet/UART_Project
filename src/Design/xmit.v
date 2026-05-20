c036541cd69e9205022d1450a4d2ec4d7725bee4
VigneshCShet <vigneshshet1@gmail.com>
2026-05-20 13:54:50 +0530

    Refactor xmit module for clarity and functionality
    Commented out unused code and adjusted state machine logic.

--- src/Design/xmit.v
+++ src/Design/xmit.v
@@ -1,20 +1,19 @@
-`default_nettype none
 module xmit #(parameter data_width = 8, s0 = 0, s1 = 1, s2 = 2, s3 = 3)(baud_clk, sys_rst_l, xmitH, xmit_dataH, uart_XMIT_dataH, xmit_doneH, xmit_active);
   input wire baud_clk, sys_rst_l, xmitH;
   input wire [data_width - 1: 0] xmit_dataH;
   output reg uart_XMIT_dataH, xmit_doneH, xmit_active;
   
- // wire xmitH_pulse;
+  //wire xmitH_pulse;
 
   //registers
-  //reg xmitH_d;
+ // reg xmitH_d;
   reg [data_width - 1 : 0] mem;
   reg [2:0] cnt;       
   reg [3:0] tick_cnt;  // Counts 16 baud_clk cycles per transmitted bit
   reg [1:0] cs, ns;
   
   // Edge detector for the start signal
- // assign xmitH_pulse = xmitH & ~xmitH_d;
+  //assign xmitH_pulse = xmitH & ~xmitH_d;
     
   always @(posedge baud_clk or negedge sys_rst_l) begin
     if(!sys_rst_l) begin // Reset all registers and counters
@@ -25,7 +24,7 @@ module xmit #(parameter data_width = 8, s0 = 0, s1 = 1, s2 = 2, s3 = 3)(baud_clk
       tick_cnt <= 0;
     end
     else begin
-    //  xmitH_d <= xmitH; //edge detection for xmitH signal
+      //xmitH_d <= xmitH; //edge detection for xmitH signal
       cs <= ns; // Update current state to next state on each clock cycle
 
      
@@ -43,13 +42,14 @@ module xmit #(parameter data_width = 8, s0 = 0, s1 = 1, s2 = 2, s3 = 3)(baud_clk
 
       
       if(cs != ns) begin // Reset bit counter on state change
-        if (ns == s2 && cs == s1) 
+        /*if (ns == s2 && cs == s1) 
           cnt <= 0; // Reset bit count when entering data state
         else if (cs != s2)
-          cnt <= 0;
+          cnt <= 0;*/
       end
-      else if(cs == s2 && tick_cnt == 15) begin
+      else if(tick_cnt == 15) begin
         cnt <= cnt + 1;
+        if(cs == s2)
         mem <= {1'b0, mem[data_width - 1:1]}; // Shift data right
       end
     end
@@ -90,7 +90,7 @@ module xmit #(parameter data_width = 8, s0 = 0, s1 = 1, s2 = 2, s3 = 3)(baud_clk
         xmit_active = 1;
         
         // Move to stop bit after sending all bits and completing the final tick
-        if(tick_cnt == 15 && cnt == data_width - 1)
+        if(cnt == data_width - 1)
           ns = s3;
         else
           ns = s2;
@@ -101,10 +101,17 @@ module xmit #(parameter data_width = 8, s0 = 0, s1 = 1, s2 = 2, s3 = 3)(baud_clk
         xmit_doneH = 0;
         xmit_active = 1;
         
-        
         if(tick_cnt == 15) begin
-          ns = s0;
-          xmit_doneH = 1;
+          if(xmitH) begin
+            ns = s1;
+            xmit_doneH = 1;
+            mem = xmit_dataH;
+          end
+          else begin
+            ns = s0;
+            xmit_doneH = 1;
+          end
+          
         end
         else
           ns = s3;
