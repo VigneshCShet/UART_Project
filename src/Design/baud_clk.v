7810d849dabc381dbef0faea44e1e2f680b8fcd3
VigneshCShet <vigneshshet1@gmail.com>
2026-05-25 21:24:07 +0530

    Correct baud_clk module formatting and reset logic
    Fix formatting issues and ensure proper module closure.

--- src/Design/baud_clk.v
+++ src/Design/baud_clk.v
@@ -1,16 +1,18 @@
-`default_nettype none
 module baud_clk #(parameter baud_rate = 2400, clk_freq = 50000000)(clk, rst, clk_out);
   input wire clk, rst;
   output reg clk_out;
- //register
+  
   reg [31:0] cnt;
+  
   
   localparam max = clk_freq / (baud_rate * 16 * 2); // Calculate the number of system clock cycles for half a baud period at 16x oversampling
   
   always @(posedge clk or negedge rst) begin
+  
     if(!rst) begin // Reset counter and output clock on reset
       cnt <= 0;
       clk_out <= 0;
+      
     end
     
     else begin
@@ -23,5 +25,7 @@ module baud_clk #(parameter baud_rate = 2400, clk_freq = 50000000)(clk, rst, clk
       else
         cnt <= cnt + 1; // Increment the counter on each clock cycle
     end
+    
   end
+  
 endmodule
