e1848b1dfffadbb62a5c7820bef085d8b81cb9f4
VigneshCShet <vigneshshet1@gmail.com>
2026-05-25 21:25:22 +0530

    Refactor baud_clk module for better readability
    Refactor counter declaration and improve code clarity.

--- src/Design/baud_clk.v
+++ src/Design/baud_clk.v
@@ -1,22 +1,19 @@
 module baud_clk #(parameter baud_rate = 2400, clk_freq = 50000000)(clk, rst, clk_out);
   input wire clk, rst;
   output reg clk_out;
-  
-  reg [31:0] cnt;
-  
-  
+    
   localparam max = clk_freq / (baud_rate * 16 * 2); // Calculate the number of system clock cycles for half a baud period at 16x oversampling
+
+  reg [$clog2(max) - 1 : 0] cnt;
   
   always @(posedge clk or negedge rst) begin
   
     if(!rst) begin // Reset counter and output clock on reset
       cnt <= 0;
       clk_out <= 0;
-      
     end
     
     else begin
-    
       if(cnt == (max - 1)) begin 
         clk_out <= ~clk_out; // Toggle the output clock
         cnt <= 0;
@@ -25,7 +22,5 @@ module baud_clk #(parameter baud_rate = 2400, clk_freq = 50000000)(clk, rst, clk
       else
         cnt <= cnt + 1; // Increment the counter on each clock cycle
     end
-    
   end
-  
 endmodule
